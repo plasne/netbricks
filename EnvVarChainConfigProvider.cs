816efb4bd6b1c5152812f4d9e9c624c1e799ee17
Andrew Vineyard <andrewsdoing@gmail.com>
2024-09-23 15:05:30 -0500

    Add many docstrings

--- EnvVarChainConfigProvider.cs
+++ EnvVarChainConfigProvider.cs
@@ -1,7 +1,15 @@
 namespace NetBricks;
 
+/// <summary>
+/// Get the value of the first environment variable that exists given a list of keys.
+/// </summary>
 public class EnvVarChainConfigProvider : IConfigProvider
 {
+    /// <summary>
+    /// Get the value of the first environment variable that exists.
+    /// </summary>
+    /// <param name="keys">A string formatted </param>
+    /// <returns></returns>
     public string Get(string keys)
     {
         var arr = keys.AsArray(() => new string[] { });
