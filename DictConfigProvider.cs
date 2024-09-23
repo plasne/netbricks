d8abeb641f567f2c4707a0c8b82b530f4e025e29
Andrew Vineyard <andrewsdoing@gmail.com>
2024-09-23 15:13:11 -0500

    Couple more

--- DictConfigProvider.cs
+++ DictConfigProvider.cs
@@ -2,15 +2,31 @@ using System.Collections.Generic;
 
 namespace NetBricks;
 
+/// <summary>
+/// Use a dictionary as a configuration provider.
+/// </summary>
 public class DictConfigProvider : IConfigProvider
 {
+    /// <summary>
+    /// The dictionary of values.
+    /// </summary>
     private Dictionary<string, string> values = new Dictionary<string, string>();
 
+    /// <summary>
+    /// Get the value of a key from the dictionary.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
     public string Get(string key)
     {
         return this.values.ContainsKey(key) ? this.values[key] : null;
     }
 
+    /// <summary>
+    /// Add a key-value pair to the dictionary.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="value"></param>
     public void Add(string key, string value)
     {
         this.values[key] = value;
--- IConfigProvider.cs
+++ IConfigProvider.cs
@@ -1,6 +1,14 @@
 namespace NetBricks;
 
+/// <summary>
+/// A configuration provider.
+/// </summary>
 public interface IConfigProvider
 {
+    /// <summary>
+    /// Get the value of a key.
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
     string Get(string key);
 }
